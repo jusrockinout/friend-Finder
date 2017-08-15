239ba789e1cb396f967e41080781a29520d25089
jusrockinout <gary.newton@ymail.com>
2017-08-15 18:19:42 -0400

    upload

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+    {
+        name:"Rick Sanchez",
+        photo:"https://vignette3.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest/scale-to-width-down/250?cb=20160923150728",
+        scores: ["3","5","4","1","5","5","4","3","5","4"]
+    },
+    {
+        name:"Morty Smith",
+        photo:"https://vignette4.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest/scale-to-width-down/250?cb=20170217193441",
+        scores:["4","5","2","5","2","5","1","4","5","4"]
+    },
+    {
+        name:"Mr. Poopybutthole",
+        photo:"https://vignette4.wikia.nocookie.net/rickandmorty/images/3/37/Mr_poopy_butthole.png/revision/latest/scale-to-width-down/250?cb=20150819161234",
+        scores:["3","5","4","1","2","5","4","3","5","4"]
+    },
+    {
+        name:"Birdperson",
+        photo:"https://vignette3.wikia.nocookie.net/rickandmorty/images/b/b1/Birdperson.png/revision/latest/scale-to-width-down/250?cb=20140415133633",
+        scores:["3","5","3","4","5","3","4","5","5","4"]
+    },
+    {
+        name:"Mr. Meeseeks",
+        photo:"https://vignette4.wikia.nocookie.net/rickandmorty/images/6/6c/MeeseeksHQ.png/revision/latest/scale-to-width-down/237?cb=20150930232412",
+        scores:["2","5","2","5","5","5","4","5","5","4"]
+    }
+
+];
+
+module.exports = friends;
+
---
+++ server.js
@@ -0,0 +1,18 @@
+//dependencies
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8080;
+var jasonParser = bodyParser.json()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
